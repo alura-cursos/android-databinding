e1b0a4ee4cfcfe19fe5c9b2c7d4cb79127a50b4a
Alex Felipe <alexfelipevieira@gmail.com>
2019-08-06 10:11:39 -0300

    2.2

--- app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
@@ -8,6 +8,7 @@ import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
@@ -40,9 +41,6 @@ class ListaNotasAdapter(
         RecyclerView.ViewHolder(viewDataBinding.root) {
 
         private lateinit var nota: Nota
-        private val campoImagem: ImageView by lazy {
-            itemView.item_nota_imagem
-        }
 
         init {
             itemView.setOnClickListener {
@@ -55,7 +53,6 @@ class ListaNotasAdapter(
         fun vincula(nota: Nota) {
             this.nota = nota
             viewDataBinding.setVariable(BR.nota,nota)
-            campoImagem.carregaImagem(nota.imagemUrl)
         }
 
     }
@@ -70,4 +67,9 @@ object DiffCallback : DiffUtil.ItemCallback<Nota>() {
 
     override fun areContentsTheSame(oldItem: Nota, newItem: Nota) = oldItem == newItem
 
+}
+
+@BindingAdapter("carregaImagem")
+fun carregaImagemPorUrl(view: ImageView, url: String){
+    view.carregaImagem(url)
 }
